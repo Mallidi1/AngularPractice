bc617b14915f22402d7cfac094f1b1cc9442f205
satishreddy <satishreddy926@gmail.com>
2025-12-28 20:48:11 +0530

    Component communication

--- src/app/_Practice/child/child.component.ts
+++ src/app/_Practice/child/child.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { log } from 'console';
+
 
 @Component({
   selector: 'app-child',
@@ -7,10 +9,28 @@ import { Component } from '@angular/core';
   styleUrl: './child.component.css'
 })
 export class ChildComponent {
+  val: any
+  d2: any = "Child";
+  message: any = 'Data from child to parent'
 
+  @Input() d1: string | undefined;
+  @Output() custome = new EventEmitter();
+
+  constructor() {
+    // console.log(this.d1);
+  }
+
+  passtoparent() {
+    this.custome.emit(this.message)
+    console.log(this.custome);
+  }
 
   goBack() {
     window.history.back();
   };
 
+  templeteref(e: HTMLInputElement) {
+    console.log(e.value);
+    // this.val = e.value
+  }
 }
--- src/app/_Practice/parent/parent.component.ts
+++ src/app/_Practice/parent/parent.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { ChildComponent } from "../child/child.component";
 
 @Component({
   selector: 'app-parent',
-  imports: [],
+  imports: [ChildComponent],
   templateUrl: './parent.component.html',
   styleUrl: './parent.component.css'
 })
 export class ParentComponent {
+
+  data1: string = "Parent Satish Reddy";
+  data2: any = 'Parent Mallidi';
+  cData: any;
+
+  constructor() {
+    console.log(this.cData);
+  }
 
 }
